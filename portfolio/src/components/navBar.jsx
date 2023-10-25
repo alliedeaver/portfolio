f9232dc294798878f05d9aeeb4c85773fcb0858b
alliedeaver <adeav97@gmail.com>
2023-10-24 23:39:35 -0700

    added files and ran the vite@latest command

---
+++ portfolio/src/components/navBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = ({ Header }) => {
+
+    function Header() {
+      return (
+        <header>
+          <h1>Alexandria Deaver</h1>
+          <p>About Me</p>
+          <p>Portfolio</p>
+          <p>Contact</p>
+          <p>Resume</p>
+        </header>
+      );
+    }
+}
+export default Header;
