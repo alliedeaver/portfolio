41d56192042d941b8fdc6ce6edd98521e901489f
alliedeaver <adeav97@gmail.com>
2023-10-26 10:04:39 -0700

    added pseudocode for future developemtn

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
-
+//at the bottom of the page with an open form to send a message 
+//have an icon for linked, github, and email  
+// email local_post_office
  const {Contact} = () => {
     return (
         <div className="card">
@@ -19,3 +21,48 @@ import React from 'react';
 }
 
 export default Contact;
+
+//for the submit form 
+
+// <div class="row">
+{/* <form class="col s12">
+<div class="row">
+  <div class="input-field col s6">
+    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+    <label for="first_name">First Name</label>
+  </div>
+  <div class="input-field col s6">
+    <input id="last_name" type="text" class="validate">
+    <label for="last_name">Last Name</label>
+  </div>
+</div>
+<div class="row">
+  <div class="input-field col s12">
+    <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+    <label for="disabled">Disabled</label>
+  </div>
+</div>
+<div class="row">
+  <div class="input-field col s12">
+    <input id="password" type="password" class="validate">
+    <label for="password">Password</label>
+  </div>
+</div>
+<div class="row">
+  <div class="input-field col s12">
+    <input id="email" type="email" class="validate">
+    <label for="email">Email</label>
+  </div>
+</div>
+<div class="row">
+  <div class="col s12">
+    This is an inline input field:
+    <div class="input-field inline">
+      <input id="email_inline" type="email" class="validate">
+      <label for="email_inline">Email</label>
+      <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+    </div>
+  </div>
+</div>
+</form>
+</div> */}
